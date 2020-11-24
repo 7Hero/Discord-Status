a959c4b4e69dacbb20be1b2123a78f46dfc82803
7Hero <49597813+7Hero@users.noreply.github.com>
2020-11-24 18:10:11 +0200

    Update DiscordStatus.js
    cleaner code

--- DiscordStatus.js
+++ DiscordStatus.js
@@ -1,34 +1,30 @@
-const axios = require('axios')
-const fs = require('fs')
+const axios = require("axios");
+const fs = require("fs");
 // const api = require("windows-api-show-window");
 // api.hideCurrentProcessWindow().then(() => {}).catch(err => {});
-function changeStatus(str,auth) {
-    axios.default.patch(
-      "https://discordapp.com/api/v6/users/@me/settings",
-      {
-        custom_status: {
-          text: str,
-        },
+function changeStatus(str, auth) {
+  axios.default.patch(
+    "https://discordapp.com/api/v6/users/@me/settings",
+    {
+      custom_status: {
+        text: str,
       },
-      {
-        headers: {
-          authorization:auth,
-        },
-      }
-    );
-  }
-string = fs.readFileSync("text.txt","utf-8")
-status = string.split("\r\n")
+    },
+    {
+      headers: {
+        authorization: auth,
+      },
+    }
+  );
+}
+string = fs.readFileSync("text.txt", "utf-8");
+status = string.split("\r\n");
 i = 2;
 
-
-
-    
-    setInterval(() => { changeStatus(status[i],status[1])
-        i=i+1;
-          if(i==status.length){
-              i =1;
-          }
-          
-      }, parseInt(status[0]));
-
+setInterval(() => {
+  changeStatus(status[i], status[1]);
+  i = i + 1;
+  if (i == status.length) {
+    i = 1;
+  }
+}, parseInt(status[0]));
